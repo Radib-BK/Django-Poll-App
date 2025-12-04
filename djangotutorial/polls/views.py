25f562d5a2a8708b0babc508c3b5e1cc7a17cbd8
Radib Bin Kabir (DSi) <radib.kabir@dsinnovators.com>
2025-12-04 13:13:41 +0600

    details page added

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_list_or_404, get_object_or_404  # type: ignore
+from django.http import HttpResponse # pyright: ignore[reportMissingModuleSource]
+from django.http import Http404 # type: ignore
 from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("pub_date")[:5]
-    template = loader.get_template("polls/index.html")
+    latest_question_list = get_list_or_404(
+        Question.objects.order_by("pub_date")[:5]
+    )
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    # return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        "question" : question,
+    }
+    return render(request, "polls/details.html", context)
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the result of question {question_id}.")
